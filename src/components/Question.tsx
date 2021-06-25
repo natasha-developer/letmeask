39e0f3fcf8fd57bd738c4b5d4f3be80372c5849b
Natasha Freitas <natasha.pimentel@uol.com.br>
2021-06-25 13:04:01 -0300

    v5.0.0

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,33 +1,42 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
+import classNames from "classnames";
 
-import '../styles/question.scss';
+import "../styles/question.scss";
 
 type QuestionProps = {
-    content: string;
-    author: {
-        name: string;
-        avatar: string;
-    };
-    children?: ReactNode;
-}
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
+};
 
 export function Question({
-    content,
-    author,
-    children,
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children,
 }: QuestionProps) {
-    return (
-        <div className="question">
-            <p>{content}</p>
-            <footer>
-                <div className="user-info">
-                    <img src={author.avatar} alt={author.name} />
-                    <span>{author.name}</span>
-                </div>
-                <div>
-                    {children}
-                </div>
-            </footer>
+  return (
+    <div
+      className={classNames(
+        "question",
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered }
+      )}
+    >
+      <p>{content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
         </div>
-    );
+        <div>{children}</div>
+      </footer>
+    </div>
+  );
 }
--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,22 +1,22 @@
-import copyImg from '../assets/images/copy.svg';
+import copyImg from "../assets/images/copy.svg";
 
-import '../styles/room-code.scss';
+import "../styles/room-code.scss";
 
 type RoomCodeProps = {
-    code: string;
-}
+  code: string;
+};
 
 export function RoomCode(props: RoomCodeProps) {
-    function copyRoomCodeToClipboard() {
-        navigator.clipboard.writeText(props.code)
-    }
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+  }
 
-    return (
-        <button className="room-code" onClick={copyRoomCodeToClipboard}>
-            <div>
-                <img src={copyImg} alt="Copiar código da sala" />
-            </div>
-            <span>Sala #{props.code}</span>
-        </button>
-    );
+  return (
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copiar código da sala" />
+      </div>
+      <span>Sala #{props.code}</span>
+    </button>
+  );
 }
